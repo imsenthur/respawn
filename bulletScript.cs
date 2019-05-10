4fb1846fd765c85d1de4b33c8b566517622aaba0
imsenthur <imsenthur@gmail.com>
2019-05-10 19:19:45 +0530

    Bullet script added.

---
+++ bulletScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    public int bulletSpeed = 100;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * bulletSpeed);
+        Destroy(this.gameObject, 0.6f);
+        
+    }
+}
